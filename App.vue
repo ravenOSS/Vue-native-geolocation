0928a6da331ef2e2eec0cd23c51795f6cc620f42
ravenOSS <35054853+ravenOSS@users.noreply.github.com>
2020-02-28 21:30:07 -0700

    Imports per explo docs

---
+++ App.vue
@@ -0,0 +1,50 @@
+<template>
+  <view class="container">
+    <text>Location:</text>
+    <text>{{location.latitude}}</text>
+    <touchable-opacity :on-press="getLocation" >
+        <text>get location</text>
+    </touchable-opacity>
+  </view>
+</template>
+
+ <script>
+import Constants from "expo-constants";
+import * as Location from "expo-location";
+import * as Permissions from "expo-permissions";
+
+export default {
+  data: function() {
+    return {
+      location: {},
+      errorMessage: ""
+    };
+  },
+  methods: {
+    getLocation: function() {
+      Permissions.askAsync(Permissions.LOCATION).then(status => {
+        if (status !== "granted") {
+          errorMessage = "Permission to access location was denied";
+        }
+        Location.getCurrentPositionAsync({}).then(location1 => {
+          location = location1;
+        });
+      }).catch((err)=>{
+        console.log(err);
+     });
+    }
+  }
+};
+</script>
+
+<style>
+.container {
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+}
+.text-color-primary {
+  color: blue;
+}
+</style>
